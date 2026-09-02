19d17908af4a704f58ea32a56f5f3d6c2d86a47b
DavidEgea <114010484+DavidEgeaCalatayud@users.noreply.github.com>
2026-09-02 13:51:42 +0200

    Replace deprecated mobile safe area views

--- mobile/src/features/transactions/TransactionScreen.tsx
+++ mobile/src/features/transactions/TransactionScreen.tsx
@@ -5,12 +5,12 @@ import {
   Alert,
   FlatList,
   Pressable,
-  SafeAreaView,
   StyleSheet,
   Text,
   TextInput,
   View,
 } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 import { useAuth } from '../../auth/AuthProvider';
 import { WorkspaceNav } from '../../components/WorkspaceNav';
